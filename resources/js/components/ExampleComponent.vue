2303c4edc49b1bd7791791ed574c666763fcad0c
Nath <nathan.harnay29@gmail.com>
2021-11-21 14:01:47 +0100

    Start

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <p>
+
+            <input type="text" v-model="name" style="border:2px black solid; background-color:cyan">
+            <input type="checkbox" v-model="checkbox">
+
+        </p>
+        <p>
+
+            Bonjour {{ name }} !
+
+        </p>
+
+
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        },
+
+        data() {
+            return {
+                'name': 'nathan',
+                'checkbox':'true'
+            }
+
+
+        }
+    }
+
+</script>
